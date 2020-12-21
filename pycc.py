9d295f42bc2203a5da8087ccb356cafabc48dcdf
Omar2535 <omar2535@alumni.ubc.ca>
2020-12-21 02:20:04 -0800

    initial add subtract

--- pycc.py
+++ pycc.py
@@ -1,3 +1,4 @@
+import re
 import sys
 
 
@@ -6,7 +7,20 @@ def main():
         sys.stderr.write(f"{sys.argv[0]} invalid number of arguments\n")
         exit(1)
 
+    params = sys.argv[1]
+    numbers = re.findall(r'\d+', params)
+    operators = re.findall(r'[\/\+\-\*]', params)
+
     print("  .globl main")
     print("main:")
-    print(f"  mov ${sys.argv[1]}, %rax")
+    print(f"  mov rax, {numbers[0]}")
+
+    for i in range(0, len(operators)):
+        if operators[i] == "+":
+            print(f"  add rax, {numbers[i+1]}")
+        elif operators[i] == "-":
+            print(f"  sub rax, {numbers[i+1]}")
+        else:
+            sys.stderr.write(f"unexpected character: {operators[i]}")
+
     print("  ret")
