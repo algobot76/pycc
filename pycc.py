4d5f730dcd2337f4592c80cca1db174bfb73175c
Omar <omar2535@gmail.com>
2020-12-21 13:28:05 -0800

    2 add plus minus operators (#6)
    * initial add subtract
    
    * add plus minus operators and test
    
    * change range parameter

--- pycc.py
+++ pycc.py
@@ -1,3 +1,4 @@
+import re
 import sys
 
 
@@ -6,7 +7,20 @@ def main():
         sys.stderr.write(f"{sys.argv[0]} invalid number of arguments\n")
         exit(1)
 
+    params = sys.argv[1]
+    numbers = re.findall(r'\d+', params)
+    operators = re.findall(r'[\/\+\-\*]', params)
+
     print("  .globl main")
     print("main:")
-    print(f"  mov ${sys.argv[1]}, %rax")
+    print(f"  mov ${numbers[0]}, %rax")
+
+    for i in range(len(operators)):
+        if operators[i] == "+":
+            print(f"  add ${numbers[i+1]}, %rax")
+        elif operators[i] == "-":
+            print(f"  sub ${numbers[i+1]}, %rax")
+        else:
+            sys.stderr.write(f"unexpected character: {operators[i]}")
+
     print("  ret")
