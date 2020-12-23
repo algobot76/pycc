9d144095488d0f4d468eafe4d24c629217b6d8ff
Kaitian Xie <kaitian96@outlook.com>
2020-12-23 15:37:11 -0800

    Better workflow (#12)
    * Replace autopep8 with black
    
    * Add verions
    
    * Add mypy
    
    * Update .pre-commit-config.yaml
    
    * Use pylint
    
    * Update pylintrc
    
    * Address pylint problems
    
    * Install colors for isort
    
    * Add github actions
    
    * Update  setup.py
    
    * Fixes

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
+"""Setup script """
 from setuptools import setup
 
 setup(
     name="pycc",
     version="0.0.1",
     python_requires=">=3.8",
-    entry_points={
-        'console_scripts': [
-            'pycc = pycc:main'
-        ]
-    }
+    entry_points={"console_scripts": ["pycc = pycc:main"]},
 )
