2037238f06191c1513abc5b6c0fa9045f8b7c596
Kaitian Xie <kaitian96@outlook.com>
2021-01-09 00:29:57 -0800

    Single letter local variables (#34)
    * Update TokenKind and NodeKind
    
    * Update test.sh
    
    * Update Codegen and Node
    
    * Update Tokenizer
    
    * Update ast
    
    * Update parser
    
    * Fix typo

--- pycc/ast.py
+++ pycc/ast.py
@@ -20,7 +20,9 @@ class NodeKind(Enum):
         ND_NE:        !=
         ND_LT:        <
         ND_LE:        <=
+        ND_ASSIGN:    =
         ND_EXPR_STMT: Expression statement
+        ND_VAR:       Variable
         ND_NUM:       Integer
     """
 
@@ -33,7 +35,9 @@ class NodeKind(Enum):
     ND_NE = auto()
     ND_LT = auto()
     ND_LE = auto()
+    ND_ASSIGN = auto()
     ND_EXPR_STMT = auto()
+    ND_VAR = auto()
     ND_NUM = auto()
 
 
@@ -45,6 +49,7 @@ class Node:
         kind: The node kind.
         lhs: The left hand side of the node.
         rhs: The right hand side of the node.
+        name: The identifier of the node.
         val: The value of the node.
     """
 
@@ -52,6 +57,7 @@ class Node:
     next: Optional[Node]
     lhs: Optional[Node]
     rhs: Optional[Node]
+    name: str
     val: int
 
 
@@ -67,7 +73,7 @@ def new_binary(kind: NodeKind, lhs: Node, rhs: Node) -> Node:
         A new node with the specified LHS and RHS.
     """
 
-    return Node(kind, None, lhs, rhs, 0)
+    return Node(kind, None, lhs, rhs, "", 0)
 
 
 def new_num(val: int) -> Node:
@@ -80,7 +86,7 @@ def new_num(val: int) -> Node:
         A new node with the value.
     """
 
-    return Node(NodeKind.ND_NUM, None, None, None, val)
+    return Node(NodeKind.ND_NUM, None, None, None, "", val)
 
 
 def new_unary(kind: NodeKind, lhs: Node) -> Node:
@@ -94,4 +100,17 @@ def new_unary(kind: NodeKind, lhs: Node) -> Node:
         A new node with the LHS node.
     """
 
-    return Node(kind, None, lhs, None, 0)
+    return Node(kind, None, lhs, None, "", 0)
+
+
+def new_var_node(name: str) -> Node:
+    """Creates a new node with the given identifier.
+
+    Args:
+        name: The identifier of the node.
+
+    Returns:
+        A new node with the identifier.
+    """
+
+    return Node(NodeKind.ND_VAR, None, None, None, name, 0)
